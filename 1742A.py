9413310c1ed26eda55b5ae016f8d9a8a6ed174c1
Hrutik Kulkarni <hrutikkulkarni89@gmail.com>
2026-03-02 11:49:55 +0530

    CodeForces 1742A Solution

---
+++ 1742A.py
@@ -0,0 +1,11 @@
+t=int(input())
+for _ in range(t):
+    a,b,c=map(int,input().split())
+    if(a+b==c):
+        print("YES")
+    elif(a+c==b):
+        print("YES")
+    elif(b+c==a):
+        print("YES")
+    else:
+        print("NO")
