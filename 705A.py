889ffd6c32b07fded4401bd3591ce328787d9289
Hrutik Kulkarni <hrutikkulkarni89@gmail.com>
2026-03-10 23:43:06 +0530

    CodeForces 705A Solution

---
+++ 705A.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(1,n+1):
+    if i%2!=0:
+        print("I hate",end=" ")
+    else:
+        print("I love",end=" ")
+    
+    if i==n:
+        print("it",end=" ")
+    else:
+        print("that",end=" ")
